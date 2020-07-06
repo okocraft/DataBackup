479fc759a4803ebc06cfec040d0a64945306e4a0
Siroshun09 <siro.shun8@gmail.com>
2020-07-07 05:07:05 +0900

    [Add] UserList, UserAPIHooker

---
+++ src/main/java/net/okocraft/databackup/user/UserAPIHooker.java
@@ -0,0 +1,39 @@
+package net.okocraft.databackup.user;
+
+import net.okocraft.userapi.api.UserAPI;
+import net.okocraft.userapi.api.data.UserData;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+final class UserAPIHooker {
+
+    @NotNull
+    static Optional<UUID> getUUID(@NotNull String name) {
+        try {
+            Optional<UserData> data = UserAPI.getUserDataByName(name);
+            if (data.isPresent()) {
+                return Optional.of(data.get().getUuid());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return Optional.empty();
+    }
+
+    @NotNull
+    static Set<String> getUsers() {
+        try {
+            return UserAPI.getAllUserName();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return Collections.emptySet();
+    }
+}
---
+++ src/main/java/net/okocraft/databackup/user/UserList.java
@@ -0,0 +1,61 @@
+package net.okocraft.databackup.user;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class UserList {
+
+    public UserList() {
+    }
+
+    @SuppressWarnings("deprecation")
+    public Optional<UUID> getUUID(@NotNull String name) {
+        Optional<UUID> uuid;
+
+        if (isUserAPIEnabled()) {
+            uuid = UserAPIHooker.getUUID(name);
+        } else {
+            uuid = Optional.empty();
+        }
+
+        if (uuid.isPresent()) {
+            return uuid;
+        } else {
+
+            return Optional.of(Bukkit.getOfflinePlayer(name).getUniqueId());
+        }
+    }
+
+    public Set<String> getUsers() {
+        Set<String> users;
+
+        if (isUserAPIEnabled()) {
+            users = UserAPIHooker.getUsers();
+        } else {
+            users = Collections.emptySet();
+        }
+
+        if (users.isEmpty()) {
+            users = Arrays.stream(Bukkit.getOfflinePlayers())
+                    .map(OfflinePlayer::getName)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toSet());
+        }
+
+        return Set.copyOf(users);
+    }
+
+    private boolean isUserAPIEnabled() {
+        return Bukkit.getPluginManager().isPluginEnabled("UserAPI");
+    }
+
+}
