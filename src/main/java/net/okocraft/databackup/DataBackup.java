649c7a91001d7da4a5d972a97ab9346c7c651de1
Siroshun09 <siro.shun8@gmail.com>
2020-07-07 03:32:03 +0900

    [Add] コンフィグロード

--- src/main/java/net/okocraft/databackup/DataBackup.java
+++ src/main/java/net/okocraft/databackup/DataBackup.java
@@ -1,8 +1,24 @@
 package net.okocraft.databackup;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class DataBackup extends JavaPlugin {
 
+    private Configuration config;
 
+    @Override
+    public void onLoad() {
+        config = new Configuration(this);
+    }
+
+    @Override
+    public void onDisable() {
+        config = null;
+    }
+
+    @NotNull
+    public Configuration getConfiguration() {
+        return config;
+    }
 }
